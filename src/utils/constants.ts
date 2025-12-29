c63170b5d84cb50094dee3d1c502790ef38c817d
Cristian Moreno <cristianfmoreno95@gmail.com>
2025-12-28 23:28:39 -0500

    Final cleanup: optimize flag images and delete unused assets

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,39 +7,39 @@ export interface Language {
 }
 
 export const languages: Language[] = [
-  { 
-    id: 'es', 
-    name: 'Spanish', 
+  {
+    id: 'es',
+    name: 'Spanish',
     color: 'from-brand-sky-blue to-brand-light-green',
     imageAlt: 'Juan Diego teaching Spanish',
-    imagePath: '/colombian_flag.png'
+    imagePath: '/colombian_flag.webp'
   },
-  { 
-    id: 'en', 
-    name: 'English', 
+  {
+    id: 'en',
+    name: 'English',
     color: 'from-blue-500 to-red-500',
     imageAlt: 'Juan Diego teaching English',
-    imagePath: '/usa_flag.png'
+    imagePath: '/usa_flag.webp'
   },
-  { 
-    id: 'pt', 
-    name: 'Portuguese', 
+  {
+    id: 'pt',
+    name: 'Portuguese',
     color: 'from-green-500 to-yellow-400',
     imageAlt: 'Juan Diego teaching Portuguese',
-    imagePath: '/brasil_flag.png'
+    imagePath: '/brasil_flag.webp'
   },
-  { 
-    id: 'it', 
-    name: 'Italian', 
+  {
+    id: 'it',
+    name: 'Italian',
     color: 'from-green-600 to-red-600',
     imageAlt: 'Juan Diego teaching Italian',
-    imagePath: '/italian_flag.png'
+    imagePath: '/italian_flag.webp'
   },
-  { 
-    id: 'fr', 
-    name: 'French', 
+  {
+    id: 'fr',
+    name: 'French',
     color: 'from-blue-600 to-red-600',
     imageAlt: 'Juan Diego teaching French',
-    imagePath: '/france_flag.png'
+    imagePath: '/france_flag.webp'
   },
 ];
