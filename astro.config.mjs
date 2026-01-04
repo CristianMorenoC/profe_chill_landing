85893c1e80315e9586166ebaf44ea62d2b176ed5
Cristian Moreno <cristianfmoreno95@gmail.com>
2026-01-03 19:55:39 -0500

    hotfix/path added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://profe-chill.vercel.app',
   integrations: [
     react(),
     tailwind(),
